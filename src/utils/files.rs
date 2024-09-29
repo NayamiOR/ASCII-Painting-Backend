8d64aa1b7940ad8e0bdb0b24f96c0c5fed4e7a66
安逐悲 <38884950+NayamiOR@users.noreply.github.com>
2024-09-29 08:30:52 +0800

    Update files.rs to make project compilable
    暂时清空save_file函数以通过编译

--- src/utils/files.rs
+++ src/utils/files.rs
@@ -3,10 +3,10 @@ use tokio::io::AsyncReadExt;
 use crate::error::Error;
 
 pub(crate) fn save_file(file: &mut File, path: &str) -> Result<(), Error> {
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).map_err(|e| Error::Io(Box::new(e)))?;
-    let mut file = File::create(path).map_err(|e| Error::Io(Box::new(e)))?;
-    file.write_all(&buffer).map_err(|e| Error::Io(Box::new(e)))?;
+    // let mut buffer = Vec::new();
+    // file.read_to_end(&mut buffer).map_err(|e| Error::Io(Box::new(e)))?;
+    // let mut file = File::create(path).map_err(|e| Error::Io(Box::new(e)))?;
+    // file.write_all(&buffer).map_err(|e| Error::Io(Box::new(e)))?;
     todo!("保存文件函数");
     Ok(())
 }
